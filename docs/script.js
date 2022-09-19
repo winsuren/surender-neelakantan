dd976f549532eda76622b5e165e0bf78e7655d0b
Surender FL <freelancer.surender@gmail.com>
2022-09-19 23:51:35 +0530

    small height mobile fixes

--- docs/script.js
+++ docs/script.js
@@ -11,22 +11,28 @@ $(document).ready(function () {
         resetActive();
         $("#profile-nav-item").addClass("active");
       }
-  
-      if ($(window).scrollTop() > $(".experiences-container").offset().top - 2) {
+
+      if (
+        $(window).scrollTop() >
+        $(".experiences-container").offset().top - 2
+      ) {
         resetActive();
         $("#experience-nav-item").addClass("active");
       }
-  
+
       if ($(window).scrollTop() > $(".skills-container").offset().top - 2) {
         resetActive();
         $("#skill-nav-item").addClass("active");
       }
-  
-      if ($(window).scrollTop() > $(".achievements-container").offset().top - 2) {
+
+      if (
+        $(window).scrollTop() >
+        $(".achievements-container").offset().top - 2
+      ) {
         resetActive();
         $("#achievement-nav-item").addClass("active");
       }
-  
+
       if ($(window).scrollTop() > $(".contact-container").offset().top - 2) {
         resetActive();
         $("#contact-nav-item").addClass("active");
@@ -37,27 +43,33 @@ $(document).ready(function () {
       } else {
         $("#nav-bar").removeClass("fixed-nav");
       }
-  
+
       if ($(window).scrollTop() > $(".profile-container").offset().top - 32) {
         resetActive();
         $("#profile-nav-item").addClass("active");
       }
-  
-      if ($(window).scrollTop() > $(".experiences-container").offset().top - 32) {
+
+      if (
+        $(window).scrollTop() >
+        $(".experiences-container").offset().top - 32
+      ) {
         resetActive();
         $("#experience-nav-item").addClass("active");
       }
-  
+
       if ($(window).scrollTop() > $(".skills-container").offset().top - 32) {
         resetActive();
         $("#skill-nav-item").addClass("active");
       }
-  
-      if ($(window).scrollTop() > $(".achievements-container").offset().top - 32) {
+
+      if (
+        $(window).scrollTop() >
+        $(".achievements-container").offset().top - 32
+      ) {
         resetActive();
         $("#achievement-nav-item").addClass("active");
       }
-  
+
       if ($(window).scrollTop() > $(".contact-container").offset().top - 32) {
         resetActive();
         $("#contact-nav-item").addClass("active");
@@ -71,7 +83,6 @@ $(document).ready(function () {
       $(".achievements-container").css("height", window.outerHeight + "px");
       $(".contact-container").css("height", window.outerHeight + "px");
     }
-  
   });
 
   function resetActive() {
@@ -80,5 +91,71 @@ $(document).ready(function () {
     $("#skill-nav-item").removeClass("active");
     $("#achievement-nav-item").removeClass("active");
     $("#contact-nav-item").removeClass("active");
+  }
+
+  if (window.outerHeight < 800) {
+    $(".scroll-down").css("top", "-80px");
+    $(".section-title").css("fontSize", "22px");
+    $(".section-sub-title").css("fontSize", "20px");
+    $(".photo-container").css("width", "154px");
+    $(".photo-container").css("height", "154px");
+    $(".photo-container img").css("width", "150px");
+    $(".photo-container img").css("height", "150px");
+    $(".profile-right").css("padding-top", "0px");
+    $(".profile-right").css("fontSize", "15px");
+    $(".profile-right").css("lineHeight", "26px");
+    $(".work-experience").css("padding", "25px");
+    $(".work-item").css("fontSize", "14px");
+    $(".work-company").css("fontSize", "15px");
+    $(".work-duration").css("fontSize", "12px");
+    $(".work-item-2").css("top", "150px");
+    $(".work-item-2-circle").css("top", "150px");
+    $(".work-item-3").css("top", "275px");
+    $(".work-item-3-circle").css("top", "275px");
+    $(".work-item-4").css("top", "400px");
+    $(".work-item-4-circle").css("top", "400px");
+    $(".skill-item").css("fontSize", "16px");
+    $(".skill-item").css("marginTop", "25px");
+    $(".sub-title").css("marginBottom", "20px");
+    $(".sub-title").css("fontSize", "18px");
+    $(".award-text").css("fontSize", "14px");
+    $(".certification-text").css("fontSize", "14px");
+  } 
+  
+  if (window.outerHeight < 700) {
+    $(".scroll-down").css("top", "-80px");
+    $(".section-title").css("fontSize", "20px");
+    $(".section-sub-title").css("fontSize", "18px");
+    $(".photo-container").css("width", "134px");
+    $(".photo-container").css("height", "134px");
+    $(".photo-container img").css("width", "130px");
+    $(".photo-container img").css("height", "130px");
+    $(".profile-right").css("padding-top", "0px");
+    $(".profile-right").css("fontSize", "14px");
+    $(".profile-right").css("lineHeight", "23px");
+    $(".work-experience").css("padding", "20px");
+    $(".work-item").css("fontSize", "13px");
+    $(".work-company").css("fontSize", "14px");
+    $(".work-duration").css("fontSize", "11px");
+    $(".work-item-2").css("top", "125px");
+    $(".work-item-2-circle").css("top", "125px");
+    $(".work-item-3").css("top", "250px");
+    $(".work-item-3-circle").css("top", "250px");
+    $(".work-item-4").css("top", "375px");
+    $(".work-item-4-circle").css("top", "375px");
+    $(".skill-item").css("fontSize", "15px");
+    $(".skill-item").css("marginTop", "20px");
+    $(".sub-title").css("marginBottom", "15px");
+    $(".sub-title").css("fontSize", "16px");
+    $(".award-text").css("fontSize", "13px");
+    $(".certification-text").css("fontSize", "13px");
+    $(".profile-container").css("padding", "20px 20px");
+    $(".experiences-container").css("padding", "20px 20px");
+    $(".skills-container").css("padding", "20px 20px");
+    $(".achievements-container").css("padding", "20px 20px");
+    $(".contact-container").css("padding", "20px 20px");
+    $(".social img").css("margin", "0px 20px");
+    $(".social img").css("width", "40px");
+    $(".skills-container .section-content").css("width", "300px");
   }
 });
